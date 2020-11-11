6fe83ca8c1411fe7f2537fe091784bf1800aba98
billyw1030 <billyw1030@gmail.com>
2020-11-11 00:05:32 -0500

    Almost Finalized both Start and Game Over Screens
    The start screen looks amazing, 10 out of 10. Added a navigational script as well for the menus (start, game over, and probably our pause menu).

---
+++ CIS267_FinalProject/Assets/Scripts/MenuNavigation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuNavigation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void loadGame()
+    {
+        SceneManager.LoadScene("Level1");
+        //Debug.Log("ButtonClicked");
+    }
+
+    public void exitGame()
+    {
+        //Only works on build of game
+        Application.Quit();
+    }
+
+    public void settings()
+    {
+
+    }
+}
