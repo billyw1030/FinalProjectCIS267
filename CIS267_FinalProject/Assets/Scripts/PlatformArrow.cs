f431eeaa8c7e6909be3dad2cad0f396134cf839f
billyw1030 <billyw1030@gmail.com>
2020-11-23 01:17:02 -0500

    Designed Collisions for platform arrows, also made them disappear after 10 seconds (which feels kind of long), I also made a prefab for the key powerup which includes an animation for it. My platform arrow is saved as a separate prefab for ethan to use in the arrow selector.

---
+++ CIS267_FinalProject/Assets/Scripts/PlatformArrow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformArrow : MonoBehaviour
+{
+    public int destroyTimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(this.gameObject, destroyTimer);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnCollisionEnter2D(Collision2D platformArrowCollision)
+    {
+        if (gameObject.CompareTag("Wall") || gameObject.CompareTag("Platform"))
+        {
+            gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY;
+        }
+
+    }
+
+
+}
