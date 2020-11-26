626ca870431ccb660b52dcb44d4f537f7b4310b4
GreatPleb <74260014+GreatPleb@users.noreply.github.com>
2020-11-26 14:28:11 -0500

    Level 2 work

---
+++ CIS267_FinalProject/Assets/Scripts/Arrows/ZiplineArrow.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZiplineArrow : MonoBehaviour
+{
+    public GameObject chain;
+    public GameObject arrow;
+    public float clock;
+    public bool startcount;
+    public float waitTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        clock = 0;
+
+        startcount = false;
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (startcount)
+        {
+            clock += Time.deltaTime;
+
+            if (clock >= waitTime)
+            {
+                clock = 0;
+                GameObject c = Instantiate(chain) as GameObject;
+                clock = 0;
+                //c.transform.position = arrow.position;
+            }
+        }
+
+    }
+    
+
+}
---
+++ CIS267_FinalProject/Assets/Scripts/sheildenemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sheildenemy : MonoBehaviour
+{
+    public GameObject box;
+    public GameObject enemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Box"))
+        {
+            Destroy(box);
+            Destroy(enemy);
+        }
+    }
+}
