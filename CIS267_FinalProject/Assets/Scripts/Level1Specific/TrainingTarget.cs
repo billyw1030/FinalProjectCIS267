ec6a93a4aaff0cbf7ed925047d4fcc27cc244e37
billyw1030 <billyw1030@gmail.com>
2020-11-27 20:33:14 -0500

    Moved Platform arrow and fixed spawn of arrow, adjusted some layering/targets

---
+++ CIS267_FinalProject/Assets/Scripts/Level1Specific/TrainingTarget.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainingTarget : MonoBehaviour
+{
+    private int targetHealth = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D trainingTarget)
+    {
+        if (trainingTarget.gameObject.CompareTag("Arrow"))
+        {
+            targetHealth -= 1;
+
+            if (targetHealth == 0)
+            {
+                Destroy(this.gameObject);
+            }
+
+        }
+    }
+}
