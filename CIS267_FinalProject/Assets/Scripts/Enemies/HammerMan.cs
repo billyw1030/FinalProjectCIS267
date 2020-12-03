e787fe7dbf90c55f44e78011387e54b71f1da212
billyw1030 <billyw1030@gmail.com>
2020-12-03 01:42:17 -0500

    Simple adjustments to my HammerMan LVL 1 Enemy

---
+++ CIS267_FinalProject/Assets/Scripts/Enemies/HammerMan.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HammerMan : MonoBehaviour
+{ 
+        public float walkSpeed = 2.0f;
+        public float wallLeft = 0.0f;
+        public float wallRight = 5.0f;
+        float walkingDirection = 1.0f;
+        Vector3 walkAmount;
+
+        // Update is called once per frame
+        void Update()
+        {
+            //walkAmount.x = walkingDirection * walkSpeed * Time.deltaTime;
+
+            //if (walkingDirection > 0.0f && transform.position.x >= wallRight)
+            //{
+            //    walkingDirection = -1.0f;
+            //}
+            //else if (walkingDirection < 0.0f && transform.position.x <= wallLeft)
+            //{
+            //    walkingDirection = 1.0f;
+            //    transform.Translate(walkAmount);
+            //}
+
+        }
+}
