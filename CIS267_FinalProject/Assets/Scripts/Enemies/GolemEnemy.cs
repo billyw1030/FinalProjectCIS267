1513e0a2caaf853456a0191d1203f975b2fc79ce
GreatPleb <74260014+GreatPleb@users.noreply.github.com>
2020-12-03 22:19:32 -0500

    Golem Animation/Script problem detecting arrow collision

---
+++ CIS267_FinalProject/Assets/Scripts/Enemies/GolemEnemy.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GolemEnemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+   
+    private Rigidbody2D GolemRigid;
+    private Animator GolemAnim;
+    private float timeStart = 0;
+    private bool Startcount=false;
+    private GameObject Arrow;
+    public GameObject box;
+    public GameObject enemy;
+
+
+
+    void Start()
+    {
+        GolemAnim = GameObject.Find("GolemSprite").GetComponent<Animator>();
+        GolemRigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Startcount == true)
+        {
+            timeStart += Time.deltaTime;
+            if(timeStart >= 1)
+            {
+                GolemAnim.SetBool("IsHit", false);
+                Startcount = false;
+                timeStart = 0;
+                Destroy(Arrow);
+            }
+
+
+
+        }
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        
+        if (collision.gameObject.CompareTag("Arrow"))
+        {
+            
+            GolemAnim.SetBool("IsHit", true);
+            //Destroy(collision.gameObject);
+            Startcount = true;
+            Arrow = collision.gameObject;
+        }
+        if (collision.gameObject.CompareTag("Box"))
+        {
+            Destroy(box);
+            Destroy(enemy);
+        }
+    }
+
+}
+    
+
+
